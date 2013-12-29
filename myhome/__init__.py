98d74ecec0e4df13d13d500b7115809bd7021dad
Jonathan Scott <jonathanscott1989@gmail.com>
2013-12-29 16:02:28 +0000

    init

---
+++ myhome/__init__.py
@@ -0,0 +1,51 @@
+import requests
+import re
+import json
+
+inside_match = re.compile(r'inside: [0-9\.]+')
+outside_match = re.compile(r'outside: [0-9\.]+')
+target_match = re.compile(r'target: [0-9\.]+')
+
+class IncorrectUsernameOrPassword(Exception):
+	pass
+
+class MyHome(object):
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		self.session = requests.session()
+		
+		login_success = self.session.post('https://myhome.britishgas.co.uk/myhome/login/', data={'username': self.username, 'password': self.password}).text
+		if 'The email address or password is incorrect, please try again' in login_success:
+			raise IncorrectUsernameOrPassword()
+
+	def get(self):
+		html = self.session.get('https://myhome.britishgas.co.uk/myhome2/climate/').text.replace('\n', ' ')
+		inside_temperature = float(inside_match.search(html).group().split(' ')[-1])
+		outside_temperature = float(outside_match.search(html).group().split(' ')[-1])
+		try:
+			target_temperature = float(target_match.search(html).group().split(' ')[-1])
+		except:
+			target_temperature = 0.0
+		heating_on = 'heatingOn: true' in html
+		print html
+		return {"inside_temperature": inside_temperature, "outside_temperature": outside_temperature, "target_temperature": target_temperature, "heating_on": heating_on}
+
+	def set_manual(self):
+		self.session.put('https://myhome.britishgas.co.uk/myhome2/climate', data=json.dumps({'heatingControl': "MANUAL"}))
+
+	def set_schedule(self):
+		self.session.put('https://myhome.britishgas.co.uk/myhome2/climate', data=json.dumps({'heatingControl': "SCHEDULE"}))
+
+	def set_off(self):
+		self.session.put('https://myhome.britishgas.co.uk/myhome2/climate', data=json.dumps({'heatingControl': "OFF"}))
+
+	def set_target(self, target):
+		self.session.put('https://myhome.britishgas.co.uk/myhome2/climate', data=json.dumps({'target': target}))
+
+
+
+if __name__ == '__main__':
+	myhome = MyHome(USERNAME, PASSWORD)
+	myhome.set_target(20.0)
+	print myhome.get()
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myhome',
+    version="0.2",
+    license="MIT",
+
+    install_requires = [
+        "requests",
+    ],
+
+    description='Screen scraping based API for British Gas MyHome',
+    long_description=open('README.txt').read(),
+
+    author='Jonathan Scott',
+    author_email='jonathan@jscott.me',
+
+    url='http://github.com/jscott1989/myhome',
+    download_url='http://github.com/jscott1989/myhome/downloads',
+
+    include_package_data=True,
+
+    packages=['myhome'],
+
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
